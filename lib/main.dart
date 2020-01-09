e194585990c48130183d799e5d9f19f72daf2535
shashwatxx <shashwatmishra2111@gmail.com>
2020-01-10 00:29:12 +0530

    Webview Boilerplate

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebviewScaffold(
+        url: "https://form.jotform.com/200084835359458",
+        withJavascript: true,
+        clearCookies: true,
+        clearCache: true,
+      ),
+    );
+  }
+}
